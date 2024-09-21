873d1dffbfb6e729402de17a36314bebd4e9ed32
Owen Lindsey <Owen.lindsey98@outlook.com>
2024-09-21 13:12:05 -0700

    adding anagram program to code directory

--- Code/SieveOfEratosthenes/ConsoleApp1/Program.cs
+++ Code/SieveOfEratosthenes/ConsoleApp1/Program.cs
@@ -1,46 +1,40 @@
-﻿namespace ConsoleApp1
+﻿using System;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             // maximum data set to iterate through is 100
-            int maximum = 100;
-
-
-            bool[] prime = new maximum(maximum + 1);
-
-            // iterate through all values from maxium and set them to true 
-            for (int i = 0; i < maximum; i++)
+            int maximum = 100000000;
+            bool[] prime = new bool[maximum + 1];
+            // iterate through all values from maximum and set them to true 
+            for (int i = 0; i <= maximum; i++)
             {
-                // 
                 prime[i] = true;
-
             }
-
-            // assume 0 and 1 are known false becasue they do not result in prime 
+            // assume 0 and 1 are known false because they do not result in prime 
             prime[1] = false;
             prime[0] = false;
-
             //iterate from 2 to the square root of the limit
-            for (int p = 2; p * p <= maximum; ++p)
+            for (int p = 2; p * p <= maximum; p++)
             {
-                // if p is prime mark multiples as prime
+                // if p is prime mark multiples as not prime
                 if (prime[p])
                 {
                     for (int i = p * p; i <= maximum; i += p)
                     {
-                        maximum[i] = false;
+                        prime[i] = false;
                     }
                 }
-                for (int i = 2; i <= maximum; i++)
+            }
+            for (int i = 2; i <= maximum; i++)
+            {
+                if (prime[i])
                 {
-                    if (maximum[i])
-                    {
-                        Console.WriteLine(i);
-                    }
+                    Console.WriteLine(i);
                 }
-
             }
         }
     }
--- Code/SieveOfEratosthenes/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Code/SieveOfEratosthenes/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3198fe836f1257b1e4d4f2d66f58fc3d9aab0624")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
